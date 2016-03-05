2709c1a883ae175a5e1a9aa22dea4d917eca53cb
Ramesh Mhetre <ramesh@web-flight.nl>
2016-03-05 15:46:45 +0530

    #comment User controller added

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function index() {
+
+    }
+}
